9bdc44e90df1c977d3954882f083bc63805fdd4c
ellapotter <egpotter@uiowa.edu>
2026-04-27 14:57:11 -0500

    Stripe now works on IOS

--- ridermobile/ios/Runner/SceneDelegate.swift
+++ ridermobile/ios/Runner/SceneDelegate.swift
@@ -2,5 +2,24 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
+  override func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    super.scene(scene, willConnectTo: session, options: connectionOptions)
+    syncWindowForPlugins()
+  }
 
+  override func sceneDidBecomeActive(_ scene: UIScene) {
+    super.sceneDidBecomeActive(scene)
+    syncWindowForPlugins()
+  }
+
+  private func syncWindowForPlugins() {
+    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+      return
+    }
+    appDelegate.window = window
+  }
 }
